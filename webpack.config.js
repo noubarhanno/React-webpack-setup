cb65c152e4cc7413b9f1a7544d09c5be3f6785d5
Noubar <noubar@Noubars-MacBook-Pro.local>
2022-05-10 10:35:17 +0400

    adding more configuration to the webpack for the devserver and the loaders for the css sass and images

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
-const htmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: path.resolve(__dirname, "src/index.js"),
@@ -8,9 +9,33 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
-    new htmlWebpackPlugin({
+    new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "index.html",
     }),
   ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+      port: 3000,
+      // open is when we call the devServer open the app in the browser page
+      open: true,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.s[ca]ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpeg|jpg|gif|png)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
